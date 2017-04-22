2ae2917b9a2fc368b3931a86bb4928fc22c265bc
SoloVeniaASaludar <daysbizarre@gmail.com>
2017-04-22 11:50:54 +0200

    +

--- start.sh
+++ start.sh
@@ -8,7 +8,8 @@ case $1 in
 	"service") SCRIPT=${TOP}/mycroft/messagebus/service/main.py ;;
 	"skills") SCRIPT=${TOP}/mycroft/skills/main.py ;;
 	"skill_container") SCRIPT=${TOP}/mycroft/skills/container.py ;;
-	"voice") SCRIPT=${TOP}/mycroft/client/speech/main.py ;;
+#	"voice") SCRIPT=${TOP}/mycroft/client/speech/main.py ;;
+	"voice") SCRIPT=${TOP}/mycroft/client/lspeech/main.py ;;
 	"cli") SCRIPT=${TOP}/mycroft/client/text/main.py ;;
 	"audiotest") SCRIPT=${TOP}/mycroft/util/audio_test.py ;;
 	"collector") SCRIPT=${TOP}/mycroft_data_collection/cli.py ;;
